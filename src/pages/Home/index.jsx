d8fb37e3940b353c6da733d7e995749d4e6e730c
mcsordi <matheuscarissordi@gmail.com>
2024-10-24 10:20:30 -0300

    console.log enviroment variable

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,19 +2,21 @@
 import { useEffect } from "react";
 import Banner from "../../Components/Banner";
 import Carrousel from "../../Components/Carrousel";
-import Spinn from "../../Components/Spinner";
+// import Spinn from "../../Components/Spinner";
 import { useFetchVideos } from "../../Components/Videos";
 
 function Home() {
 const fetchVideos = useFetchVideos()
+
+console.log(import.meta.env.VITE_DATABASE_CATEGORIES)
   
 useEffect(()=>{
   fetchVideos
 },[])
 const listVideos = fetchVideos && fetchVideos.map((el)=>el);
-  if (!listVideos) {
-    return <Spinn />;
-  }
+  // if (!listVideos) {
+  //   return <Spinn />;
+  // }
   return (
     <>
       <Banner image="favoritos" />
