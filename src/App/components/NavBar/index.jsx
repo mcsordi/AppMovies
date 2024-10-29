a345331e809a331383f5b7530bc900adc91646de
mcsordi <matheuscarissordi@gmail.com>
2024-10-29 08:58:42 -0300

    mobile favBar problem solved

--- src/App/components/NavBar/index.jsx
+++ src/App/components/NavBar/index.jsx
@@ -1,17 +1,17 @@
 import P from "prop-types"
 import { Link } from "react-router-dom"
-function NavBar({ config, clicked }) {
+function NavBar({ config, onClick }) {
 
   return (
     <nav className={`${config} text-md bg-black gap-6 uppercase font-semibold`}>
-      <Link onClick={() => { clicked(true), clicked(false) }} to="/">Home</Link>
-      <Link onClick={() => { clicked(true), clicked(false) }} to="/search">Pesquisar</Link>
-      <Link onClick={() => { clicked(true), clicked(false) }} to="/favorites">Favoritos</Link>
+      <Link onClick={() => { return onClick(true), onClick(false) }} to="/">Home</Link>
+      <Link onClick={() => { return onClick(true), onClick(false) }} to="/search">Pesquisar</Link>
+      <Link onClick={() => { return onClick(true), onClick(false) }} to="/favorites">Favoritos</Link>
     </nav>
   )
 }
 NavBar.propTypes = {
   config: P.string,
-  clicked: P.func
+  onClick: P.func
 }
 export default NavBar
